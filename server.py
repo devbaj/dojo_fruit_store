d8281524429f28ec7eac044826bdd9bd0a5be5f4
Brett Jory <bjory7@gmail.com>
2019-04-02 16:20:48 -0700

    finished assignment

--- server.py
+++ server.py
@@ -1,18 +1,28 @@
 from flask import Flask, render_template, request, redirect
-app = Flask(__name__)  
+app = Flask(__name__)
 
-@app.route('/')         
+@app.route('/')
 def index():
     return render_template("index.html")
 
-@app.route('/checkout', methods=['POST'])         
+@app.route('/checkout', methods=['POST'])
 def checkout():
-    print(request.form)
-    return render_template("checkout.html")
 
-@app.route('/fruits')         
+    strawS = request.form["strawberry"]
+    raspS = request.form["raspberry"]
+    appleS = request.form["apple"]
+    first_nameS = request.form["first_name"]
+    last_nameS = request.form["last_name"]
+    idS = request.form["student_id"]
+    fruit_count = int(strawS) + int(raspS) + int(appleS)
+    print("*"*30)
+    print(f"Charging {first_nameS} {last_nameS} for {fruit_count} fruits")
+    print("*"*30)
+    return render_template("checkout.html", strawT=strawS, raspT=raspS, appleT=appleS, first_nameT=first_nameS, last_nameT=last_nameS, idT=idS)
+
+@app.route('/fruits')
 def fruits():
     return render_template("fruits.html")
 
-if __name__=="__main__":   
-    app.run(debug=True)    
+if __name__=="__main__":
+    app.run(debug=True)
